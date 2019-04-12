ecd580172a50eabdfcc493b97547b36f282faceb
Michael Mottola <mottola64@gmail.com>
2019-04-12 03:49:28 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,8 @@
 class Author
 attr_accessor :name, :post
 
+@@post_count = 0
+
 def initialize(name)
   @name = name
   @@posts = []
@@ -13,6 +15,7 @@ end
 def add_post(post)
 @@posts << post
 post.author = self
+@@post_count += 1
 end
 
 def add_post_by_name
